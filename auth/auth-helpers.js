2afa698c66c53c70a1cf8030d471b47330fd5655
matt kersner <mattkersner@gmail.com>
2017-07-13 09:19:10 -0400

    updated engines node version in package.json

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -21,7 +21,8 @@ function createUser(req, res) {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
-    dob: req.body.dob
+    dob: req.body.dob,
+    address: req.body.address
   });
 }
 
